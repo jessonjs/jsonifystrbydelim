0dd84d7d8895df9d217136b6119ddfb8a3773856
Jess Brisson <jessbrisson@gmail.com>
2022-01-27 21:22:59 -0600

    update approach taken to something simple

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function jsonifyString(str, delim = '.') {
     if (strArr.length === 1) {
         return {[strArr[0]]: null};
     }
+    /*
+     * the initial approach
+     *
     const mapToObject = (o1, o2) => {
         const o1Keys = Object.keys(o1);
         if (o1[o1Keys[0]] != null) {
@@ -22,6 +25,13 @@ function jsonifyString(str, delim = '.') {
             return o1;
         })
     );
+    */
+
+    // a much simpler approach
+    const closedString = '{'  + strArr.map((v, i) => {
+        return `"${v}"${i + 1 === strArr.length ? ': null' : ''}`
+    }).join(':{') + new Array(strArr.length).fill('}').join('');
+    return JSON.parse(closedString);
 }
 
 module.exports = jsonifyString;
